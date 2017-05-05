57ff1873f368b8f9a5cb6422e6eb65671b81026a
Grafopostroitel <ilyna.lora@gmail.com>
2017-05-05 17:49:36 +0700

    Create layout.R

---
+++ layout.R
@@ -0,0 +1,50 @@
+layout <- list(
+  autosize = FALSE, 
+  dragmode = "zoom", 
+  height = 480, 
+  hovermode = "x", 
+  legend = list(
+    x = -0.0127018591959, 
+    y = 1.2059562595, 
+    orientation = "h", 
+    traceorder = "normal", 
+    xanchor = "center", 
+    yanchor = "top"
+  ), 
+  margin = list(
+    r = 100, 
+    t = 80, 
+    b = 80, 
+    l = 100
+  ), 
+  paper_bgcolor = "rgb(23, 190, 207)", 
+  plot_bgcolor = "rgb(227, 119, 194)", 
+  showlegend = TRUE, 
+  title = "Click to enter Plot title", 
+  width = 640, 
+  xaxis = list(
+    autorange = TRUE, 
+    domain = c(0, 1), 
+    range = c(-23.1261261261, 389.126126126), 
+    showspikes = TRUE, 
+    title = "days", 
+    type = "linear"
+  ), 
+  yaxis = list(
+    autorange = TRUE, 
+    range = c(-574.5, 315.5), 
+    showspikes = TRUE, 
+    title = "temp", 
+    type = "linear"
+  ), 
+  yaxis2 = list(
+    anchor = "x", 
+    autorange = TRUE, 
+    overlaying = "y", 
+    range = c(-9.92780748663, 150.927807487), 
+    showspikes = TRUE, 
+    side = "right", 
+    title = "popopop\", 
+    type = "linear"
+  )
+)
